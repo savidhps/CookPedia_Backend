c0da47a625dbe55cbdc235dbef4abba4b1f4b044
savidhps <savidhps00@gmail.com>
2025-06-04 16:12:38 +0530

    added registerController -complete registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,24 @@
 //register
+const users=require('../models/userModel')
+
 exports.registerController=async(req,res)=>{
+    const {username,email,password}=req.body
+    console.log(username,email,password);
+    try{
+        const existingUser=await users.findOne({email})
+        if(existingUser){
+            res.status(401).json('Aulredy Exist')
+        }else{
+            const newUser=new users({
+                username,
+                email,
+                password
+            })
+            await newUser.save()
+            res.status(200).json(newUser)
+        }
+    }catch(error){
+        res.status(500).json(error)
+    }
     
 }
